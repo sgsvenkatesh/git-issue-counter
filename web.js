b233db8d76da329d272d824886fc40b61d505050
SGS Venkatesh <venki.iitbbs@gmail.com>
2015-09-09 01:34:56 +0530

    build files

---
+++ web.js
@@ -0,0 +1,10 @@
+/**
+ * Created by sgsvenkatesh on 9/9/15.
+ */
+var gzippo = require('gzippo');
+var express = require('express');
+var app = express();
+
+app.use(express.logger('dev'));
+app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+app.listen(process.env.PORT || 5000);
